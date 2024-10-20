fbae528c396bf9c747c339b751fa714d97e10085
japiirainen <joona.piirainen@gmail.com>
2024-10-20 15:12:46 +0300

    chore(flake): update inputs

--- flake.nix
+++ flake.nix
@@ -10,30 +10,18 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        agda-stdlib = ps: (ps.standard-library.overrideAttrs (_: {
-          version = "2.0";
-          src = pkgs.fetchFromGitHub {
-            repo = "agda-stdlib";
-            owner = "agda";
-            rev = "177dc9e983606b653a3c6af2ae2162bbc87882ad";
-            sha256 = "sha256-ovnhL5otoaACpqHZnk/ucivwtEfBQtGRu4/xw4+Ws+c=";
-          };
-        }));
-        ghc = pkgs.haskell.packages.ghc94.ghcWithPackages (ps: with ps; [ clock ]);
+        ghc = pkgs.haskell.packages.ghc98.ghcWithPackages (ps: with ps; [ clock ]);
         agda = pkgs.agda.withPackages {
           inherit ghc;
-          pkgs = ps: [ (agda-stdlib ps) ];
+          pkgs = ps: [ ps.standard-library ];
         };
       in
       {
         devShells.default = pkgs.mkShell {
-          packages = (with pkgs; [ marksman agda ghc ]) ++
-            (with pkgs.haskellPackages; [ text containers ]);
+          packages = (with pkgs; [ marksman ghc agda ]);
           shellHook = ''
-            echo "---------- japiirainen's note taking environment ------------"
-            ${agda}/bin/agda --version
             ${ghc}/bin/ghc --version
-            echo "-------------------------------------------------------------"
+            ${agda}/bin/agda --version
           '';
         };
       }
